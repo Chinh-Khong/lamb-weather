67e9e711542269306570764ffae0aa10233e2e92
Chinh Khong <khongchinh757@gmail.com>
2025-03-10 17:01:56 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import HeaderWeather from './components/header-weather/Header.tsx';
+import React from "react";
+
+const App = () => {
+  const _renderWeatherCity = () => (
+    <div className='pt-10 pb-10 px-10 flex flex-col gap-4 text-[#003870] md:min-w-[550px] shadow-xl rounded-[12px] border border-gray-300 bg-white'>
+      <p className='font-bold text-[24px]'>Dự báo thời tiết Hà Nội</p>
+      <p>Hôm nay, 07/03/2025</p>
+      <div className='flex flex-col gap-4'>
+        <div>
+          <div className='flex flex-row gap-6 items-center'>
+            Img
+            <p>20 độ</p>
+            <div className='flex flex-col gap-4'>
+              <p>Bầu trời Quang Đãng</p>
+              <p>Cảm giác như 19 độ</p>
+            </div>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="w-full overflow-x-auto sm:overflow-hidden">
+          <div className="flex flex-row gap-2 whitespace-nowrap max-w-[300px]">
+            <div>Thấp/Cao</div>
+            <div>Thấp/Cao</div>
+            <div>Thấp/Cao</div>
+            <div>Thấp/Cao</div>
+            <div>Thấp/Cao</div>
+            <div>Thấp/Cao</div>
+            <div>Thấp/Cao</div>
+
+          </div>
+        </div>
+      </div>
+    </div >
+  );
+  const _renderWeatherBuoi = () => (
+    <div className='pt-10 pb-16 md:px-10 w-[96%] px-[15px] flex flex-col gap-4 text-[#003870] max-w-[550px] shadow-xl rounded-[12px] border border-gray-300 bg-white'>
+      <p className='font-bold text-[24px]'>Nhiệt độ Hà Nội</p>
+      <div className='inline-grid md:grid-cols-4 grid-cols-2 gap-[8px]'>
+        <div className='flex flex-col justify-between items-center gap-8 border-r-1 border-gray-100 pr-2'>
+          <p className='font-[600]'>Ngày</p>
+          <div>Img</div>
+          <p>16 độ / 15 độ</p>
+        </div>
+        <div className='flex flex-col justify-between items-center gap-8 border-r-1 border-gray-100 pr-2'>
+          <p className='font-[600]'>Ngày</p>
+          <div>Img</div>
+          <p>16 độ / 15 độ</p>
+        </div>
+        <div className='flex flex-col justify-between items-center gap-8 border-r-1 border-gray-100 pr-2'>
+          <p className='font-[600]'>Ngày</p>
+          <div>Img</div>
+          <p>16 độ / 15 độ</p>
+        </div>
+        <div className='flex flex-col justify-between items-center gap-8 border-r-1 border-gray-100 pr-2'>
+          <p className='font-[600]'>Ngày</p>
+          <div>Img</div>
+          <p>16 độ / 15 độ</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const _renderWeatherHourl = () => (
+    <div className='w-full md:px-50 bg-red-400 '>
+      <p>Dự báo thời tiết Hà Nội những ngày tới</p>
+      <div className = "flex flex-row gap-4 items-center">
+        <p>Hôm nay</p>
+        <p>18 ddooj / 24 độ</p>
+        <div className="flex flex-row gap-2 items-center">
+          <p>img</p>
+          <p>Nắng đẹp</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="App w-full h-full">
+      <HeaderWeather />
+      <div className='w-full bg-[#82b6e9] h-12 shadow-lg'></div>
+      <div
+        className='bg-cover bg-center bg-no-repeat pt-10 pb-16 flex flex-col md:flex-row justify-center items-center gap-8'
+        style={{ backgroundImage: "url('/image/bg-home-lancapse.jpg')" }}
+      >
+        {_renderWeatherCity()}
+        {_renderWeatherBuoi()}
+      </div>
+      {_renderWeatherHourl()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header-weather/Header.tsx
@@ -0,0 +1,54 @@
+import { ClockCircleOutlined, SearchOutlined } from "@ant-design/icons";
+import { Image } from "antd";
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+
+const HeaderWeather = () => {
+  const [currentTime, setCurrentTime] = useState(dayjs());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='w-full px-2 bg-[#098d4b] py-3 md:py-6 md:px-50 flex flex-row md:justify-between items-center gap-4'>
+      <div className='flex flex-row md:gap-38 gap-3'>
+        <img
+          src="./weather-logo.png"
+          width={45}
+          height={45}
+        />
+        <div className='md:min-w-[500px] bg-[#ffffff14] rounded-md flex flex-row justify-between items-center'>
+          <input
+            placeholder="Tìm kiếm"
+            className='bg-[#ffffff14] w-full h-full border-none px-4 placeholder-white focus:outline-none text-white'
+          />
+
+          <div className='px-2 bg-[#ffffff14] h-full flex justify-center items-center'>
+            <div className='py-2 px-4 flex items-center justify-center cursor-pointer bg-[#098d4b] rounded-sm'>
+              <SearchOutlined style={{ color: 'white', fontWeight: '600' }} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-white flex items-center gap-2">
+        <span className="hidden sm:inline"><ClockCircleOutlined /> Giờ địa phương:</span>
+        <div className="flex flex-col md:flex-row">
+          <span className="font-semibold md:min-w-[65px]">
+            {currentTime.format('HH:mm:ss')}
+          </span>
+          <span className="font-semibold">
+            {currentTime.format('DD/MM/YYYY')}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderWeather;
